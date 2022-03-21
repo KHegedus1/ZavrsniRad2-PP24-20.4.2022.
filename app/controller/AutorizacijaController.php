c5ef02bc4ee475d5d3e73768d58d9b6644bd976f
KHegedus1 <kico.kiki@gmail.com>
2022-03-21 18:31:54 +0100

    Create AutorizacijaController.php

---
+++ app/controller/AutorizacijaController.php
@@ -0,0 +1,18 @@
+<?php
+
+abstract class AutorizacijaController extends Controller
+{
+    
+
+    public function __construct()
+    {
+        parent::__construct();
+        if(!isset($_SESSION['autoriziran'])){
+            $this->view->render('login',[
+                'email'=>'',
+                'poruka'=>'Prijavite se molim.'
+            ]);
+            exit;
+        }
+    }
+}
