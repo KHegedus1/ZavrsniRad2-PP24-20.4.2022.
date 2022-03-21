c677926e0dd2deee52ee7f640890b9165bacef9b
KHegedus1 <kico.kiki@gmail.com>
2022-03-21 17:56:23 +0100

    Update Proizvod.php

--- app/model/Proizvod.php
+++ app/model/Proizvod.php
@@ -18,12 +18,6 @@ class Proizvod
         $veza = DB::getInstanca();
         $izraz = $veza->prepare('
         
-        select a.sifra, a.zanr, a.izvodac, a.naziv, a.cijena, a.izdavackaKuca, a.zaliha,
-        count(b.sifra) as kosarica
-        from proizvod a left join kosarica b
-        on a.sifra=b.proizvod
-        group by a.sifra, a.zanr, a.izvodac, a.naziv, a.cijena, a.izdavackaKuca, a.zaliha
-        order by 3;
         
         ');
         $izraz->execute();
@@ -34,8 +28,8 @@ class Proizvod
         $veza = DB::getInstanca();
         $izraz = $veza->prepare('
         
-        insert into proizvod (zanr,izvodac,naziv,cijena,izdavackaKuca,zaliha)
-        values (:zanr,:izvodac,:naziv,:cijena,:izdavackaKuca,:zaliha);
+        insert into proizvod (naziv,cijena,kategorija,sifra)
+        values (:naziv,:cijena,:kategorija,:sifra);
         
         ');
         $izraz->execute($parametri);
@@ -46,12 +40,9 @@ class Proizvod
         $izraz = $veza->prepare('
         
         update proizvod set 
-            zanr=:zanr,
-            izvodac=:izvodac,
             naziv=:naziv,
             cijena=:cijena,
-            izdavackaKuca=:izdavackaKuca,
-            zaliha=:zaliha
+            kategorija=kategorija,
             where sifra=:sifra;
         
         ');
