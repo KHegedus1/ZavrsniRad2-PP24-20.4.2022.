499e75487ed2bb8038e8a32e3f64d2a3eeab74e1
KHegedus1 <kico.kiki@gmail.com>
2022-04-18 20:01:20 +0200

    nova sifra

--- app/konfiguracija.php
+++ app/konfiguracija.php
@@ -16,7 +16,7 @@
           'server'=>'localhost',
           'baza'=>'furije_webshop',
           'korisnik'=>'furije_webshop',
-          'lozinka'=>'%bZ)Aq]Qznl0'
+          'lozinka'=>'h_@0x.j[_-KF'
       ];
   }
   
