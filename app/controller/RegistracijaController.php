846c780eff46fc50cfee3aee3559374c62357d71
KHegedus1 <kico.kiki@gmail.com>
2022-04-18 14:39:29 +0200

    dorada registracije

--- app/controller/RegistracijaController.php
+++ app/controller/RegistracijaController.php
@@ -1,6 +1,27 @@
 <?php
   class RegistracijaController extends Controller
   {
+    public function __construct()
+    {
+        parent::__construct();
+        
+        $this->kupac = new stdClass();
+        $this->kupac->sifra = null;
+        $this->kupac->ime = '';
+        $this->kupac->prezime = '';
+        $this->kupac->ulica = '';
+        $this->kupac->kucniBroj = '';
+        $this->kupac->grad = '';
+        $this->kupac->email = '';
+
+        $this->poruka = new stdClass();
+        $this->poruka->ime='';
+        $this->poruka->prezime='';
+        $this->poruka->ulica = '';
+        $this->poruka->kucniBroj = '';
+        $this->poruka->grad = '';
+        $this->poruka->email = '';
+    }
       public function index()
       {
           $this->regView('Popunite podatke','');
